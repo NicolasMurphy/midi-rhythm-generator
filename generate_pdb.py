b20d27703f94f45c5ed0f722ff05966609df1ec9
Nicolas Murphy <murphynick10@gmail.com>
2024-09-21 20:45:55 -0500

    experimenting with pdb file

---
+++ generate_pdb.py
@@ -0,0 +1,30 @@
+from mido import MidiFile, MidiTrack, Message
+from Bio.PDB import PDBParser
+
+parser = PDBParser()
+structure = parser.get_structure("Molecule_1", "HMDB0001830 (Progesterone)_3D.pdb")
+
+mid = MidiFile()
+track = MidiTrack()
+mid.tracks.append(track)
+
+for model in structure:
+    for chain in model:
+        for residue in chain:
+            for atom in residue:
+                x, y, z = atom.get_coord()
+
+                note = int((x % 1) * 127)
+                note = max(0, min(note + 60, 127))
+
+                velocity = int((y % 1) * 127)
+                velocity = max(0, min(velocity, 127))
+
+                duration = int((z % 1) * 480) + 60
+
+                track.append(Message("note_on", note=note, velocity=velocity, time=0))
+                track.append(
+                    Message("note_off", note=note, velocity=velocity, time=duration)
+                )
+
+mid.save("Progesterone.mid")
