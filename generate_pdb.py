0389c2ae1406809f9d1b721e580d399c38ffe81f
Nicolas Murphy <murphynick10@gmail.com>
2024-09-23 20:48:40 -0500

    Convert all pdb files in root

--- generate_pdb.py
+++ generate_pdb.py
@@ -1,30 +1,40 @@
+import os
 from mido import MidiFile, MidiTrack, Message
 from Bio.PDB import PDBParser
 
 parser = PDBParser()
-structure = parser.get_structure("Calcitriol", "HMDB0001903 (Calcitriol)_3D.pdb")
+pdb_files = [f for f in os.listdir(".") if f.endswith(".pdb")]
 
-mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
+for pdb_file in pdb_files:
+    file_name = os.path.splitext(pdb_file)[0]
+    structure = parser.get_structure(file_name, pdb_file)
 
-for model in structure:
-    for chain in model:
-        for residue in chain:
-            for atom in residue:
-                x, y, z = atom.get_coord()
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
 
-                note = int((x % 1) * 127)
-                note = max(0, min(note + 60, 127))
+    for model in structure:
+        for chain in model:
+            for residue in chain:
+                for atom in residue:
+                    x, y, z = atom.get_coord()
 
-                velocity = int((y % 1) * 127)
-                velocity = max(0, min(velocity, 127))
+                    note = int((x % 1) * 127)
+                    note = max(0, min(note + 60, 127))
 
-                duration = int((z % 1) * 480) + 60
+                    velocity = int((y % 1) * 127)
+                    velocity = max(0, min(velocity, 127))
 
-                track.append(Message("note_on", note=note, velocity=velocity, time=0))
-                track.append(
-                    Message("note_off", note=note, velocity=velocity, time=duration)
-                )
+                    duration = int((z % 1) * 480) + 60
 
-mid.save("Calcitriol.mid")
+                    track.append(
+                        Message("note_on", note=note, velocity=velocity, time=0)
+                    )
+                    track.append(
+                        Message("note_off", note=note, velocity=velocity, time=duration)
+                    )
+
+    midi_file_name = f"{file_name}.mid"
+    mid.save(midi_file_name)
+
+    print(f"Converted {pdb_file} to {midi_file_name}")
